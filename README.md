9a1c8f40e9f41eed10254fee64a08fdb1a098f1a
Lurkerc <434848237@qq.com>
2018-04-11 10:51:30 +0800

    Restructure

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-This repository contains the specifications.
+This repository contains the specifications.  
+page: [https://codespec.lookapi.org](https://codespec.lookapi.org)  
 
-- [HTML编码规范](html.md) <span class="std-rec">[1.0]</span>
-- [CSS编码规范](css.md) <span class="std-rec">[1.0]</span>
-- [Less编码规范](less.md) <span class="std-rec">[1.0]</span>
-- [Javascript编码规范](javascript.md) <span class="std-rec">[1.0]</span>
-- [ECMAScript编码规范](es.md) <span class="std-rec">[1.0]</span>
-- [E-JSON数据传输标准](e-json.md) <span class="std-rec">[1.0]</span>
-- [模块和加载器规范](module.md) <span class="std-rec">[1.0]</span>
+- [HTML编码规范](docs/html.md) <span class="std-rec">[1.0]</span>
+- [CSS编码规范](docs/css.md) <span class="std-rec">[1.0]</span>
+- [Less编码规范](docs/less.md) <span class="std-rec">[1.0]</span>
+- [Javascript编码规范](docs/javascript.md) <span class="std-rec">[1.0]</span>
+- [ECMAScript编码规范](docs/es.md) <span class="std-rec">[1.0]</span>
+- [E-JSON数据传输标准](docs/e-json.md) <span class="std-rec">[1.0]</span>
+- [模块和加载器规范](docs/module.md) <span class="std-rec">[1.0]</span>
---
+++ static/config.js
@@ -0,0 +1,46 @@
+self.$config = {
+  nav: [
+    {
+      title: 'Html',
+      path: '/',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/html.md'
+    },
+    {
+      title: 'Css',
+      path: '/css',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/css.md'
+    },
+    {
+      title: 'Less',
+      path: '/less',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/less.md'
+    },
+    {
+      title: 'Javascript',
+      path: '/javascript',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/javascript.md'
+    },
+    {
+      title: 'ECMAScript',
+      path: '/es',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/es.md'
+    },
+    {
+      title: 'E-Json',
+      path: '/e-json',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/e-json.md'
+    },
+    {
+      title: '模块和加载器',
+      path: '/module',
+      source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/docs/module.md'
+    }
+  ],
+  icons: [
+    {
+      icon: 'github',
+      label: 'Contribute on GitHub',
+      link: 'https://github.com/Lurkerc/codeSpec'
+    }
+  ]
+}
